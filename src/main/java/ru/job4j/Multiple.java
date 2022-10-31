9e7f991b716c45d0cccdaacb6354985f43bb497e
redgad13 <vadim_kuzin@list.ru>
2022-10-31 15:49:42 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class Multiple {
     public static void main(String[] args) {
         for (int i = 1; i <10 ; i++) {
-            System.out.println("1 * " + i + " = " + i);
+            System.out.println("1 * " + i + " = " + i );
         }
     }
 }
--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Vadim Kuzin");
-        System.out.println("13/04/1988");
+        System.out.println("13.04.1988");
     }
 }
