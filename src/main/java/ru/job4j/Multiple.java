f791e6f068c419a7885f40bc2704effa3bdd1e88
redgad13 <vadim_kuzin@list.ru>
2022-10-31 15:45:55 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        for (int i = 1; i <10 ; i++) {
+            System.out.println("1 * " + i + " = " + i);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vadim Kuzin");
+        System.out.println("13/04/1988");
+    }
+}
