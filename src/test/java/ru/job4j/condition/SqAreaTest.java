27d5a5f9309d94e16cfccebad4d7fdd9edede371
redgad13 <vadim_kuzin@list.ru>
2022-11-08 15:34:41 +0300

    4.5. Отладка программы в IDEA [#159464]

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -1,36 +1,25 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class SqAreaTest {
-
+public class SqAreaTest {
     @Test
-    public void whenP6K2Then2() {
+    public void whenP6K2Square2() {
+        int expected = 2;
         int p = 6;
-        double k = 2.0;
-        double expected = 2.0;
-        double out = SqArea.square(6, 2);
-        Assert.assertEquals(expected, out, 0.01);
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 
     @Test
-    public void whenP10K3dot3Then4dot46() {
-        int p = 10;
-        double k = 3.3;
-        double expected = 4.46;
-        double out = SqArea.square(10, 3.3);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void whenP6K11Then0dot68() {
-        int p = 6;
-        double k = 11;
-        double expected = 0.68;
-        double out = SqArea.square(6, 11);
-        Assert.assertEquals(expected, out, 0.01);
+    public void whenP4K1Square1() {
+        int expected = 1;
+        int p = 4;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 }
