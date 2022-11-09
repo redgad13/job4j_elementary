b63eaa1f37e66037c7a9ae8b3e09f6250b249593
redgad13 <vadim_kuzin@list.ru>
2022-11-09 16:08:51 +0300

    6.0.1. Объявление массива. [#3613]

--- src/main/java/ru/job4j/loop/DoWhileExample.java
+++ src/main/java/ru/job4j/loop/DoWhileExample.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class DoWhileExample {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // объект для считывания числа с консоли
-        int password; // переменная для сохранения полученного числа от пользователя
-        do { // открываем цикл
-            System.out.println("Введите пароль: "); // вывод сообщения до проверки
-            password = scanner.nextInt(); // сохраняем введенный пользователем пароль
-        } while (password != 555); // повторяем блок do{}, пока пароль не равен 555
-        System.out.println("Пароль введён успешно!"); // выводим на печать подтверждение введения правильного пароля
+        Scanner scanner = new Scanner(System.in);
+        int password;
+        do {
+            System.out.println("Введите пароль: ");
+            password = scanner.nextInt();
+        } while (password != 555);
+        System.out.println("Пароль введён успешно!");
     }
 }
