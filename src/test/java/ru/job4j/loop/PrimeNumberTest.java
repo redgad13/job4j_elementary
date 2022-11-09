60003491075c0085b6366d6b06a5bdff4e1658e7
redgad13 <vadim_kuzin@list.ru>
2022-11-09 12:15:31 +0300

    5.6. Простые числа [#156314]

---
+++ src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PrimeNumberTest {
+    @Test
+    void when5() {
+        int num = 5;
+        int out = PrimeNumber.count(num);
+        int expected = 3;
+        assertThat(out).isEqualTo(expected);
+    }
+
+    @Test
+    void when11(){
+        int num = 11;
+        int out = PrimeNumber.count(num);
+        int expected = 5;
+        assertThat(out).isEqualTo(expected);
+    }
+
+    @Test
+    void when2(){
+        int num = 2;
+        int out = PrimeNumber.count(num);
+        int expected = 1;
+        assertThat(out).isEqualTo(expected);
+    }
+}
